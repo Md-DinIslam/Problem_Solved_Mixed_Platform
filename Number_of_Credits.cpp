f50cad73738525567e7ea6455f2f012eda82195b
Muhammad Dinislam <108214424+Md-DinIslam@users.noreply.github.com>
2023-01-06 23:09:56 +0600

    Add files via upload

---
+++ Number_of_Credits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        int sum = 0;
+        sum = (x * 4) + (y * 2) + (z * 0);
+        cout<<sum<<endl;
+    }
+}

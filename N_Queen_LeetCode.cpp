24ca8aac2b8631cfe3badfa12f3fa23054bae517
Muhammad Dinislam <108214424+Md-DinIslam@users.noreply.github.com>
2023-05-09 03:12:53 +0600

    Add files via upload

---
+++ N_Queen_LeetCode.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+
+// Speed
+#define Code ios_base::sync_with_stdio(false);
+#define By cin.tie(NULL);
+#define Din cout.tie(NULL);
+
+// Aliases
+using ll = long long;
+using lld = long double;
+using ull = unsigned long long;
+
+// TypeDEf
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef vector<vll> mat;
+typedef unordered_map<ll, ll> umpll;
+typedef unordered_map<int, int> umpi;
+typedef map<ll, ll> mpll;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> oset;
+
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define rep(i,a, n) for (int i = a; i <= n; i++)
+#define rfl(i, m, n) for (int i = n; i >= m; i--)
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pn cout << "NO\n";
+#define pf cout << "FIRST\n";
+#define nn cout << "\n";
+#define bitcount __builtin_popcountll
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.end(), v.begin()
+
+//DEBUG
+void __print(int x) {cerr << x << " ";}
+void __print(long x) {cerr << x << " ";}
+void __print(long long x) {cerr << x << " ";}
+void __print(unsigned x) {cerr << x << " ";}
+void __print(unsigned long x) {cerr << x << " ";}
+void __print(unsigned long long x) {cerr << x << " ";}
+void __print(float x) {cerr << x << " ";}
+void __print(double x) {cerr << x << " ";}
+void __print(long double x) {cerr << x << " ";}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+// Constants
+const lld PI = 3.141592653589793238;
+const ll INF = 1e18 + 9;
+const ll mod = 1e9 + 7;
+const int mxN = 5e5 + 10;
+vector<string> v;
+vector<vector<string>> ans;
+bool canPlace(int row, int col, int n) {
+    for (int i = 0; i < row; ++i) {
+        if (v[i][col] == 'Q') return false;
+    }
+    int i = row, j = col;
+    while (i >= 0 && j >= 0) {
+        if (v[i][j] == 'Q') return false;
+        --i, --j;
+    }
+    i = row, j = col;
+    while (i >= 0 && j < n) {
+        if (v[i][j] == 'Q') return false;
+        --i, ++j;
+    }
+    return true;
+}
+void nQueen(int n, int i = 0) {
+    if (i == n) {
+        ans.push_back(v);
+        return;
+    }
+    for (int j = 0; j < n; ++j) {
+        if (canPlace(i, j, n)) {
+            v[i][j] = 'Q';
+            nQueen(n, i + 1);
+            v[i][j] = '.';
+        }
+    }
+    return;
+}
+vector<vector<string>> solveNQueens(int n) {
+    for (int i = 0; i < n; ++i) {
+        string s;
+        for (int j = 0; j < n; ++j) {
+            s.push_back('.');
+        }
+        v.push_back(s);
+    }
+    nQueen(n);
+    return ans;
+}
+void printAll() {
+    for (auto &X : ans) {
+        for (auto &x : X) {
+            cout << x << '\n';
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<vector<string>> board = solveNQueens(n);
+    printAll();
+    __print(board);
+}
+// Main
+int main()
+{
+    Code By Din
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    clock_t z = clock();
+    // ll t; cin >> t;
+    // while(t--) solve();
+    solve();
+    // fl(i,t) //Kickstart
+    // {
+    //     cout<<"Case #"<<i+1<<": ";
+    //     solve(); nn
+    // }
+    cerr << "\nRun Time : " << (((double)(clock() - z)) / CLOCKS_PER_SEC);
+    return 0;
+}
